f70670334d5bc844af0c15573f87bb4053fa3fc2
budotding1025 <budotding1025@users.noreply.github.com>
2026-09-04 09:07:59 +0800

    Add family passphrase cloud sync via Firebase Realtime Database

---
+++ cloud-config.js
@@ -0,0 +1,6 @@
+// 家庭云同步配置（家长填一次即可，所有设备打开网站会自动读到）
+// 把 Firebase Realtime Database 地址填到 databaseURL
+// 例如："https://your-project-default-rtdb.firebaseio.com"
+window.RED_ALERT_CLOUD = {
+  databaseURL: ""
+};
